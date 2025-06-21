535bc479df5c0fc410625a5bd5f570adfe8deea9
Barclay Leach <barclaydario@gmail.com>
2025-06-20 21:47:10 -0500

    header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import "./Header.module.scss";
+
+function Header() {
+    return (
+        <>
+        <header>
+        <h1>El Hada Artesana</h1>
+        <nav>
+            <ul>
+            <li><a href="#home">Menú</a></li>
+            <li><a href="#about">Contacto</a></li>
+            <li><a href="#contact">Registrarse</a></li>
+            </ul>
+        </nav>
+        </header>
+        <hr />
+        </>
+    );
+}
+
+export default Header;
