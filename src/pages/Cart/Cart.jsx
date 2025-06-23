3fc1209380efb44a45f47da84abdb029e03a8372
Barclay Leach <barclaydario@gmail.com>
2025-06-22 19:46:16 -0500

    4 pages ready.

---
+++ src/pages/Cart/Cart.jsx
@@ -0,0 +1,21 @@
+import "./Cart.scss";
+import { Link } from 'react-router-dom';
+import Header from "../../components/Header/Header.jsx";
+import Footer from "../../components/Footer/Footer.jsx";
+import cofee from "../../assets/cofee.png";  
+
+function Login() {
+  return (
+    <>
+      <Header />
+      <main>
+        <section>
+
+        </section>
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default Login;
